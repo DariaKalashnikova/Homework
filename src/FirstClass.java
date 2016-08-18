8c47ae913ee4adf91c8f494623538bb4b17b8a33
Daria <cat_dasha@mail.ru>
2016-08-19 01:44:42 +0300

    First homework on JavaCore

---
+++ src/FirstClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 19.08.2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("My first programms on Java...");
+    }
+}
---
+++ src/ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 19.08.2016.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("My first programms on Java...");
+    }
+}
