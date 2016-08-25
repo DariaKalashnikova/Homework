b92861abb432cfe67edc3a3a28450fff9ef53936
Daria <cat_dasha@mail.ru>
2016-08-26 01:07:55 +0300

    moved all classes from the first homework to the Module1 folder, created folder for the second Module2 and started homework

---
+++ src/Module2/Assignment1.java
@@ -0,0 +1,13 @@
+package Module2;
+
+
+public class Assignment1 {
+    public static int main(String[] args) {
+        int[] ArraySum = {6, 55, 18, 9, 15, 42, 77, 32, 98, 100};
+        int sum = 0;
+        for (int i = 0; i < ArraySum.length; i++) {
+            sum += ArraySum[i];
+        }
+
+    }
+}
