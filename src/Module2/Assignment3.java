8d5c3f774409cc01fc1df7e95a8e2caadb4daaef
DariaKalashnikova <cat_dasha@mail.ru>
2016-08-31 17:01:46 +0300

    changed

--- src/Module2/Assignment3.java
+++ src/Module2/Assignment3.java
@@ -3,5 +3,19 @@ package Module2;
 
 public class Assignment3 {
 
+    static int[]balances={680,250,995,845,312};
+    static String[]ownerNames={"John","Jack","Jill","Patrik","Bob"};
+    double commision = 0.05;
 
-}
+    public static void main(String[] args) {
+
+
+        String ownerName = "Patrik";
+        double withdraw = 500;
+
+        String ownerName1 = "Bob";
+        double withdraw1 = 310;
+
+
+    }
+    }
---
+++ src/Module2/Assignment4.java
@@ -0,0 +1,8 @@
+package Module2;
+
+
+public class Assignment4 {
+    static int[]balances={680,250,995,845,312};
+    static String[]ownerNames={"John","Jack","Jill","Patrik","Bob"};
+
+}
