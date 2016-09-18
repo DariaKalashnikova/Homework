5a1b5b01a8316ab6eb98751e304403e09b025bce
Daria <cat_dasha@mail.ru>
2016-09-19 00:16:04 +0300

    module 5 assignment 3

--- src/Module5/API.java
+++ src/Module5/API.java
@@ -3,5 +3,7 @@ package Module5;
 
 public interface API {
 
-Room[] findRooms(int price, int persons, String cityName, String hotelName);
+    Room[] findRooms(int price, int persons, String cityName, String hotelName);
+
+    Room[] getAll();
 }
--- src/Module5/BookingComAPI.java
+++ src/Module5/BookingComAPI.java
@@ -2,4 +2,14 @@ package Module5;
 
 
 public class BookingComAPI implements API {
+    @Override
+    public Room[] findRooms(int price, int persons, String cityName, String hotelName) {
+        
+        return new Room[0];
+    }
+
+    @Override
+    public Room[] getAll() {
+        return new Room[0];
+    }
 }
