b5d5779da8bb297503ae8a663f5a87cd84da6d5f
DariaKalashnikova <cat_dasha@mail.ru>
2016-09-29 17:08:45 +0300

    module 6 start

--- src/Module6/UserUtils.java
+++ src/Module6/UserUtils.java
@@ -2,7 +2,20 @@ package Module6;
 
 public class UserUtils {
 
-   // User[] uniqueUsers (User[] users){
+    User[] uniqueUsers (User[] users){
 
-   // }
+        return users;
+    }
+    User[] usersWithContitionalBalance (User[] users, int balance){
+        return users;
+    }
+    final User[] paySalaryToUsers (User[] users){
+        return users;
+    }
+    final long[] getUsersId (User[] users){
+        return getUsersId(users);
+    }
+    User[] deleteEmptyUsers (User[] users){
+        return users;
+    }
 }
