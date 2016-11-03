2ea4da3cd1d5e11517fad16118b90eba752bb810
DariaKalashnikova <cat_dasha@mail.ru>
2016-11-03 17:49:03 +0200

    module 9

--- src/Module9/Main.java
+++ src/Module9/Main.java
@@ -1,10 +1,12 @@
 package Module9;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import Module7.MethodsUsed;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -20,30 +22,29 @@ public class Main {
         User user9 = new User(10, "Dolly", "Kim", "Chicago", 10000);
         User user10 = new User(2, "Thor", "Odinov", "Valhalla", 10005230);
 
-        List<Order> order = new ArrayList<>(11);
-        order.add(new Order(15, 200, Currency.USD, "Food", "BILLA", user1));
-        order.add(new Order(74, 150, Currency.UAH, "Dwarf", "Store", user2));
-        order.add(new Order(14, 800, Currency.USD, "Fig", "Furshet", user3));
-        order.add(new Order(1, 5000, Currency.UAH, "Car", "Toystore", user4));
-        order.add(new Order(23, 309, Currency.UAH, "Shovel", "AgriWorld", user5));
-        order.add(new Order(22, 10, Currency.USD, "Lollipop", "Candy", user6));
-        order.add(new Order(19, 203, Currency.USD, "Pot", "HighSmile", user7));
-        order.add(new Order(33, 650, Currency.USD, "Pigeon", "Petstore", user8));
-        order.add(new Order(51, 30, Currency.UAH, "Cheese", "MilkFarm", user9));
-        order.add(new Order(9, 700, Currency.USD, "TorqueTester", "Miller", user10));
-        order.add(new Order(19, 203, Currency.USD, "Pot", "HighSmile", user7));
+        List<Order> orders = new ArrayList<>(11);
+        orders.add(new Order(15, 200, Currency.USD, "Food", "BILLA", user1));
+        orders.add(new Order(74, 150, Currency.UAH, "Dwarf", "Store", user2));
+        orders.add(new Order(14, 800, Currency.USD, "Fig", "Furshet", user3));
+        orders.add(new Order(1, 5000, Currency.UAH, "Car", "Toystore", user4));
+        orders.add(new Order(23, 309, Currency.UAH, "Shovel", "AgriWorld", user5));
+        orders.add(new Order(22, 10, Currency.USD, "Lollipop", "Candy", user6));
+        orders.add(new Order(19, 203, Currency.USD, "Pot", "HighSmile", user7));
+        orders.add(new Order(33, 650, Currency.USD, "Pigeon", "Petstore", user8));
+        orders.add(new Order(51, 30, Currency.UAH, "Cheese", "MilkFarm", user9));
+        orders.add(new Order(9, 700, Currency.USD, "TorqueTester", "Miller", user10));
+        orders.add(new Order(19, 203, Currency.USD, "Pot", "HighSmile", user7));
 
-        Collections.sort(order, (o1, o2) -> o2.compareTo(o1));
-        System.out.println(order);
+        Comparator<Order> comparatorPrice = (o1, o2) ->
+                Integer.compare(o2.getPrice(), o1.getPrice());
 
-        Comparator<Order> comparatorPriceAndCity = (o1, o2)->{
+
+        Comparator<Order> comparatorPriceAndCity = (o1, o2) -> {
             int result = Integer.compare(o1.getPrice(), o2.getPrice());
             if (result !=0){
                 return result;
             }return o1.getUser().getCity().compareTo(o2.getUser().getCity());
         };
-        order.sort(comparatorPriceAndCity);
-        System.out.println(order);
 
         Comparator<Order> comparatorItemShopIdAndCity = (o1, o2) -> {
             int result = o1.getItemName().compareTo(o2.getItemName());
@@ -52,12 +53,34 @@ public class Main {
             }result = o1.getShopIdentificator().compareTo(o2.getShopIdentificator());
             if (result !=0){
                 return result;
-            }
-            return o1.getUser().getCity().compareTo(o2.getUser().getCity());
+            }return o1.getUser().getCity().compareTo(o2.getUser().getCity());
         };
-        order.sort(comparatorItemShopIdAndCity);
-        System.out.println(order);
 
-        
+        orders = orders.stream().distinct().collect(Collectors.toList());
+        System.out.println(orders);
+
+        Predicate<Order> predicate = i -> i.getPrice()<1500;
+        List<Order> orderList = orders.stream().filter(predicate).collect(Collectors.toList());
+
+        boolean isContain = orders.stream().anyMatch(o -> (o.getUser().getLastName().equals("Petrov")));
+
+        List<Order> listWithoutUsd = orders.stream().filter(o -> !o.getCurrency().equals(Currency.USD)).collect(Collectors.toList());
+
+        orders.sort(comparatorPrice);
+        System.out.println(orders);
+        orders.sort(comparatorPriceAndCity);
+        System.out.println(orders);
+        orders.sort(comparatorItemShopIdAndCity);
+        System.out.println(orders);
+        System.out.println(orderList);
+        System.out.println(isContain);
+        System.out.println(listWithoutUsd);;
+        System.out.println(separateList(orders, i -> i.getUser().getCity()));
+
+
+    }
+
+    private static <T> Map<T, List<Order>> separateList (List<Order> orders, Function<Order, T> orderTFunction){
+        return orders.stream().collect(Collectors.groupingBy(orderTFunction));
     }
 }
